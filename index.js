1b78410413b91ec42ef9015cb2b27c95f4baab86
Amyrga Tavakai <amyrga.tavakay@tele2.ru>
2024-05-29 10:31:45 +0300

    fix cronObserver

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const TelegramBot = require('node-telegram-bot-api')
 const cron = require('node-cron')
 const messages = require('./src/telegram/messages.json')
-const {cronObserver} = require("./src/telegram/modules/birthday")
+const {	cronObserver	} = require("./src/telegram/modules/birthday")
 const { START } = require('./src/telegram/commands')
-const {startCheckPolling} = require("./src/telegram/utils");
+const {	startCheckPolling} = require("./src/telegram/utils")
 require('dotenv').config()
 
 const { TOKEN } = process.env
 const BOT = new TelegramBot(TOKEN, { polling: true })
 
-cron.schedule('0 01 00 * * *', cronObserver)
+cron.schedule('0 01 00 * * *', () => cronObserver(BOT))
 
 BOT.onText(START, (msg) => {
 	const chatId = msg.chat.id
