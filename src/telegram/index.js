f35c70008784262277513ccd391acb089f0cdfa8
Amyrga Tavakai <amyrga.tavakay@tele2.ru>
2024-10-01 00:54:28 +0300

    animation & stickers

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -2,23 +2,10 @@ const { cronObserver } = require('./modules/birthday')
 const { sendStatsMessage } = require('./modules/messages')
 const { updateStats } = require("./modules/messages/helpers");
 const { STATS, START } = require('./commands')
-const {isCommandType} = require("./utils");
-
-const getMediaFile = async (bot, fileId) => {
-  try {
-    const file = await bot.getFile(fileId);
-    const filePath = file;
-    // console.log(`File Path: https://api.telegram.org/file/bot${token}/${filePath}`);
-    console.log(file);
-  } catch (error) {
-    console.error('Error getting file:', error);
-  }
-}
 
 const statsObserver = async (bot, msg) => {
   const message = msg.text
   await updateStats(msg)
-  // getMediaFile(bot, msg.document.file_id)
 
   if (message === STATS) {
     await sendStatsMessage(bot, msg)
--- src/telegram/modules/messages/helpers.js
+++ src/telegram/modules/messages/helpers.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const { MESSAGE_TYPE } = require('./constants')
-const { User, TotalMessage} = require("../../../../db/models")
+const { User, TotalMessage, Animation, Stickers } = require("../../../../db/models")
 
 const getMessageType = (msg) => {
   if (msg.sticker) return MESSAGE_TYPE.sticker
@@ -14,24 +14,34 @@ const updateStats = async (msg) => {
     totalMessage.total += 1
     await totalMessage.save()
 
-    let { id: msg_author_tg_id, first_name, last_name} = msg.from
+    let { id: msg_author_tg_id, first_name, last_name } = msg.from
     let msg_type = getMessageType(msg)
 
-    const [user, created] = await User.findOrCreate({
+    const [user] = await User.findOrCreate({
       where: { tg_user_id: msg_author_tg_id },
-      defaults: {
-        tg_user_id: msg_author_tg_id
-      },
+      defaults: { tg_user_id: msg_author_tg_id },
     })
 
-    if (msg_type === MESSAGE_TYPE.sticker) user.stickers_count += 1
-    if (msg_type === MESSAGE_TYPE.animation) {
-      user.animation_count += 1
-      // console.log(msg)
+    const handleMedia = async (mediaType, model, userField, mediaField, fileId) => {
+      const [media] = await model.findOrCreate({
+        where: { file_id: fileId },
+        defaults: { file_id: fileId }
+      })
+      user[userField] += 1
+      media.send_count += 1
+      await media.save()
     }
-    user.msgs_count += 1
 
-    user.first_name = last_name ? `${first_name} ${last_name}` : first_name
+    if (msg_type === MESSAGE_TYPE.sticker) {
+      await handleMedia('sticker', Stickers, 'stickers_count', 'file_id', msg.sticker?.file_id)
+    }
+
+    if (msg_type === MESSAGE_TYPE.animation) {
+      await handleMedia('animation', Animation, 'animation_count', 'file_id', msg.animation?.file_id)
+    }
+
+    user.msgs_count += 1
+    user.first_name = last_name ? `${first_name} ${last_name}` : first_name // Обновляем имя пользователя
     await user.save()
   } catch (err) {
     console.error('Error:', err)
@@ -90,16 +100,26 @@ async function getUserAvatar(BOT, userId, token) {
   }
 }
 
+const getMediaFile = async (BOT, fileId, token) => {
+  const file = await BOT.getFile(fileId);
+  return `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+};
+
+
 const getStats = async (BOT, token) => {
   try {
+    // Получаем общие статистики
     const totalMessagesCount = await TotalMessage.findOne()
     const totalStickersCount = await User.sum('stickers_count')
     const totalAnimationsCount = await User.sum('animation_count')
 
+    // Получаем топ пользователей по количеству сообщений
     const topUsersByTotalMessages = await User.findAll({
       order: [['msgs_count', 'DESC']],
       limit: 5,
     })
+
+    // Добавляем аватарки пользователей
     const mergeTopUsersByTotalMessages = await Promise.all(
       topUsersByTotalMessages.map(async (user) => {
         const avatar = await getUserAvatar(BOT, user.tg_user_id, token)
@@ -110,11 +130,53 @@ const getStats = async (BOT, token) => {
         }
       })
     )
+
+    // Получаем топ 3 стикера
+    const topStickers = await Stickers.findAll({
+      order: [['send_count', 'DESC']],
+      raw: true,
+      limit: 3,
+    })
+
+    // Получаем URL стикеров для веб-приложения
+    const mergeTopStickers = await Promise.all(
+      topStickers.map(async (sticker) => {
+        const fileUrl = await getMediaFile(BOT, sticker.file_id, token) // Используем метод для получения URL файла
+        return {
+          file_id: sticker.file_id,
+          send_count: sticker.send_count,
+          file_url: fileUrl,
+        }
+      })
+    )
+
+    // Получаем топ 3 анимации
+    const topAnimations = await Animation.findAll({
+      order: [['send_count', 'DESC']],
+      raw: true,
+      limit: 3,
+    })
+
+    // Получаем URL анимаций для веб-приложения
+    const mergeTopAnimations = await Promise.all(
+      topAnimations.map(async (animation) => {
+        const fileUrl = await getMediaFile(BOT, animation.file_id, token) // Используем метод для получения URL файла
+        return {
+          file_id: animation.file_id,
+          send_count: animation.send_count,
+          file_url: fileUrl,
+        }
+      })
+    )
+
+    // Формируем и возвращаем итоговый ответ
     return {
       totalMessagesCount: totalMessagesCount.total,
       totalStickersCount,
       totalAnimationsCount,
       mergeTopUsersByTotalMessages,
+      topStickers: mergeTopStickers,
+      topAnimations: mergeTopAnimations,
     }
   } catch (error) {
     console.error('Error:', error)
