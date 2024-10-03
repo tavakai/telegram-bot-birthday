e022a8749db1905f904998de6509cc8f7fbc9bf6
Amyrga Tavakai <amyrga.tavakay@tele2.ru>
2024-10-03 20:54:08 +0300

    update 2.2

--- src/tests/stats.test.js
+++ src/tests/stats.test.js
@@ -1,50 +1,23 @@
-const { updateStats } = require('../telegram/modules/messages/helpers');
-const { User, Stickers, Animation } = require('../../db/models'); // ваши модели
+const { updateStats } = require('../telegram/modules/messages/helpers')
+const { User, Stickers } = require('../../db/models')
 
-describe('updateStats function', () => {
-  let mockUser, mockSticker, mockAnimation;
+test('updateStats updates the user statistics correctly', async () => {
+  // Моковые данные
+  const msg = {
+    from: { id: 123, first_name: 'John', last_name: 'Doe' },
+    sticker: { file_id: 'sticker123' },
+  }
 
-  beforeEach(() => {
-    // Моковые данные пользователя
-    mockUser = { tg_user_id: 123, stickers_count: 0, msgs_count: 0, animations_count: 0, save: jest.fn() };
+  const mockUser = { tg_user_id: 123, stickers_count: 0, msgs_count: 0, save: jest.fn() }
+  const mockSticker = { file_id: 'sticker123', send_count: 0, save: jest.fn() }
 
-    // Моковые данные для стикеров
-    mockSticker = { file_id: 'sticker123', send_count: 0, save: jest.fn() };
+  User.findOrCreate = jest.fn(() => [mockUser])
+  Stickers.findOrCreate = jest.fn(() => [mockSticker])
 
-    // Моковые данные для анимации
-    mockAnimation = { file_id: 'animation123', send_count: 0, save: jest.fn() };
+  await updateStats(msg)
 
-    // Мокируем методы findOrCreate для каждой модели
-    User.findOrCreate = jest.fn(() => [mockUser]);
-    Stickers.findOrCreate = jest.fn(() => [mockSticker]);
-    Animation.findOrCreate = jest.fn(() => [mockAnimation]);
-  });
-
-  test('should update statistics for a text message', async () => {
-    const msg = {
-      from: { id: 123, first_name: 'John', last_name: 'Doe' },
-      text: 'Hello world!',
-    };
-
-    await updateStats(msg)
-
-    // Ожидаем увеличение общего количества сообщений
-    expect(mockUser.msgs_count).toBe(1);
-    expect(mockUser.save).toHaveBeenCalled();
-  });
-
-  test('should update statistics for a sticker message', async () => {
-    const msg = {
-      from: { id: 123, first_name: 'John', last_name: 'Doe' },
-      sticker: { file_id: 'sticker123' },
-    };
-
-    await updateStats(msg)
-
-    // Ожидаем увеличение счетчика стикеров и сохранение данных пользователя и стикера
-    expect(mockUser.stickers_count).toBe(1);
-    expect(mockSticker.send_count).toBe(1);
-    expect(mockUser.save).toHaveBeenCalled();
-    expect(mockSticker.save).toHaveBeenCalled();
-  });
-});
+  expect(mockUser.stickers_count).toBe(1)
+  expect(mockSticker.send_count).toBe(1)
+  expect(mockUser.save).toHaveBeenCalled()
+  expect(mockSticker.save).toHaveBeenCalled()
+})
